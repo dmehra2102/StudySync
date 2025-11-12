965b118eae8b0271c11bb23d33a080c6743b91d8
Deepanshu Mehra <deepanshumehra2102@gmail.com>
2025-11-12 17:47:25 +0530

    feat: added user_service for registering, login and getProfile

--- go.mod
+++ go.mod
@@ -5,11 +5,12 @@ go 1.25.1
 require (
 	github.com/golang-jwt/jwt/v5 v5.3.0
 	github.com/joho/godotenv v1.5.1
+	golang.org/x/crypto v0.44.0
 	gorm.io/gorm v1.31.1
 )
 
 require (
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
-	golang.org/x/text v0.20.0 // indirect
+	golang.org/x/text v0.31.0 // indirect
 )
--- internal/service/user_service.go
+++ internal/service/user_service.go
@@ -1,8 +1,11 @@
 package service
 
 import (
+	"errors"
+
 	"github.com/dmehra2102/StudySync/internal/domain"
 	"github.com/dmehra2102/StudySync/pkg/auth"
+	"golang.org/x/crypto/bcrypt"
 )
 
 type UserService struct {
@@ -22,4 +25,73 @@ type RegisterRequest struct {
 	Password  string `json:"password" bidning:"required,min=6"`
 	FirstName string `json:"first_name" bidning:"required"`
 	LastName  string `json:"last_name" bidning:"required"`
+}
+
+type LoginRequest struct {
+	Email    string `json:"email" binding:"required,email"`
+	Password string `json:"password" binding:"required"`
+}
+
+type AuthResponse struct {
+	User  *domain.User `json:"user"`
+	Token string       `json:"token"`
+}
+
+func (s *UserService) Register(req RegisterRequest) (*AuthResponse, error) {
+	existingUser, _ := s.userRepo.FindByEmail(req.Email)
+	if existingUser != nil {
+		return nil, errors.New("user already exists with this email")
+	}
+
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(req.Password), bcrypt.DefaultCost)
+	if err != nil {
+		return nil, err
+	}
+
+	user := &domain.User{
+		Email:     req.Email,
+		Password:  string(hashedPassword),
+		FirstName: req.FirstName,
+		LastName:  req.LastName,
+		Role:      "student",
+	}
+
+	if err := s.userRepo.Create(user); err != nil {
+		return nil, err
+	}
+
+	token, err := s.jwtAuth.GenerateToken(user.ID, user.Email, user.Role)
+	if err != nil {
+		return nil, err
+	}
+
+	return &AuthResponse{
+		User:  user,
+		Token: token,
+	}, nil
+}
+
+func (s *UserService) Login(req LoginRequest) (*AuthResponse, error) {
+	user, err := s.userRepo.FindByEmail(req.Email)
+	if err != nil || user == nil {
+		return nil, errors.New("invalid credentials")
+	}
+
+	if err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(req.Password)); err != nil {
+		return nil, errors.New("invalid credentials")
+	}
+
+	token, err := s.jwtAuth.GenerateToken(user.ID, user.Email, user.Role)
+	if err != nil {
+		return nil, err
+	}
+
+	return &AuthResponse{
+		User:  user,
+		Token: token,
+	}, nil
+}
+
+func (s *UserService) GetProfile(userID uint) (*domain.User, error) {
+	return s.userRepo.FindByID(userID)
 }
